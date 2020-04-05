d6b4d1295775ff8b8d44fdde07b67919e0789427
magic929 <nyanmina929@gmail.com>
2020-04-05 15:53:16 +0800

    add the writing data into database

--- database/input.py
+++ database/input.py
@@ -1,8 +1,11 @@
 import ast
 import re
+import sys
+
+from utils.sql_ops import Sqlite
 
 
-field = ["id", "name", "base_agi", "base_rarity", "growth_board3_id", "base_pdef", "delta_agi", 
+field = ["id", "name", "chara_id", "cost", "base_agi", "base_rarity", "growth_board3_id", "base_pdef", "delta_agi", 
         "dex", "growth_board7_id", "eva", "growth_board1_id", "party_skill_type", "delta_pdef", 
         "growth_board6_id", "attribute_id", "growth_board4_id", "dress_episode_id", "base_atk", 
         "delta_atk", "auto_skill2_id", "party_skill_id", "auto_skill2_type", "delta_hp", "auto_skill1_id",
@@ -10,6 +13,15 @@ field = ["id", "name", "base_agi", "base_rarity", "growth_board3_id", "base_pdef
         "auto_skill3_type", "delta_mdef", "auto_skill3_id", "cri", "growth_board8_id", "hit", "base_mdef",
         "growth_board2_id", "growth_board9_id", "command_skill3_id", "published_at", "command_unique_skill_id",
         "dress_type", "base_hp", "description"]
+
+types = ["TEXT", "TEXT", "INTEGER", "INTEGER", "INTEGER", "INTEGER", "TEXT", "INTEGER", "INTEGER",
+        "INTEGER", "TEXT", "INTEGER", "TEXT", "INTEGER", "INTEGER",
+        "TEXT", "INTEGER", "TEXT", "TEXT", "INTEGER",
+        "INTEGER", "TEXT", "TEXT", "INTEGER", "INTEGER", "TEXT",
+        "TEXT", "INTEGER", "INTEGER", "TEXT", "TEXT",
+        "INTEGER", "INTEGER", "TEXT", "INTEGER", "TEXT", "INTEGER", "INTEGER",
+        "TEXT", "TEXT", "TEXT", "TIMESTAMP", "TEXT",
+        "INTEGER", "INTEGER", "TEXT"]
 
 
 def append_suffix(matched):
@@ -23,8 +35,6 @@ def read_file(path):
     
     data = data.replace("return", "").replace("=", ":").replace("[[", "\"").replace("]]", "\"")
     data = re.sub(r"[\[\]\. ]", "", data)
-    # with open("test.txt", "w", encoding="utf8") as f:
-    #     f.write(data)
     data = re.sub(r"(\t|:)[a-zA-z0-9_]+", append_suffix, data)
     data = re.sub(r"\s+", "", data)
     with open("test.txt", "w", encoding="utf8") as f:
@@ -34,8 +44,23 @@ def read_file(path):
     for key, value in data.items():
         tmp = [key]
         for f in field[1:]:
-            tmp.append(value[f])
+            if isinstance(value[f], dict):
+                tmp.append(value[f]['ja'])
+            else:
+                tmp.append(value[f])
         
         result.append(tuple(tmp))
     
     return result
+
+
+def insert_dress(data):
+    dress = Sqlite("starlightRe.db")
+    dress.create("dress", field, types)
+    dress.insert("dress", data)
+    dress.close()
+
+
+if __name__ == "__main__":
+    data = read_file(sys.argv[1])
+    insert_dress(data)
---
+++ utils/sql_ops.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class Sqlite():
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.c = self.conn.cursor()
+    
+    def create(self, table, cols, types):
+        ct = []
+        for c, t in zip(cols, types):
+            ct.append("{} {}".format(c, t))
+        sqls = 'CREATE TABLE IF NOT EXISTS {} ({})'.format(table, ','.join(ct))
+        try: 
+            self.c.execute(sqls)
+        except Exception as e:
+            print("error: ", e)
+            return 0            
+        return 1
+
+    def insert(self, table, data):
+        sqls = 'INSERT INTO ' + table + ' VALUES ({}?)'.format('?,'*(len(data[0]) - 1))
+        print(sqls)
+        try:
+            self.c.executemany(sqls, iter(data))
+            self.conn.commit()
+        except Exception as e:
+            print("error: ", e)
+            self.conn.rollback()
+    
+    def update(self, table, data):
+        pass
+
+    def delete(self, table):
+        pass
+    
+    def close(self):
+        self.c.close()
+        self.conn.close()
