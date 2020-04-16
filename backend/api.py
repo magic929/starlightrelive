b2be88a4e94d33162e72f241b428a15067f77bc7
magic929 <nyanmina929@gmail.com>
2020-04-16 22:26:17 +0800

    增加按钮post功能（test）

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,21 @@
 from flask import Blueprint
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from models.dress import get_id
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
-
+parser = reqparse.RequestParser(trim=True)
+parser.add_argument('streth', type=dict, action='append')  
+    
 class Spam(Resource):
     def get(self):
         dress = get_id("1030003")
         return [{'name': dress.name, 'charaId': dress.chara_id}]
-
+    
+    def post(self):
+        args = parser.parse_args()
+        print("akb48")
+        result = args["streth"]
+        print(result)
 
 api = Api(api_bp)
 api.add_resource(Spam, '/dress')
--- frontend/src/components/Dress.vue
+++ frontend/src/components/Dress.vue
@@ -4,12 +4,24 @@
         <el-table class="dress-table" :data="dressData" stripe>
         <el-table-column prop="name" label="name" width="180"></el-table-column>
         <el-table-column prop="charaId" label="charaId" width="180"></el-table-column>
+        <el-table-column label="select">
+            <template slot-scope="scope">
+                <el-checkbox v-model="scope.row.checked"></el-checkbox> 
+            </template>
+        </el-table-column>
         </el-table>
+        <el-row>
+            <el-button type="primary" size="medium" @click="PostData">确定</el-button>
+        </el-row>
     </div>
 </template>
 
 <script>
-const axios = require('axios').create()
+// const axios = require('axios').create()
+const axios = 
+    process.env.VUE_APP_RESET_SERVER ==='json-mock'
+    ? require('axios').create({baseURL: 'http://localhost:3000'})
+    : require('axios').create()
 export default {
     data(){
         return {
@@ -20,6 +32,13 @@ export default {
         updateDressData: async function (){
             const response = await axios.get('/api/dress')
             this.dressData = response.data
+        },
+        PostData: async function (){
+            let test = {"streth": [{"name": "123", "charaId": "234"}, {"name": "234", "charaId": "345"}]}
+            await axios.post('/api/dress', test)
+            .then(res=>{
+                console.log('res=>', res)
+            })
         }
     },
     mounted(){
