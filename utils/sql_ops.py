b7e06e547e37b34d7670fa82433538293f4a1565
magic929 <nyanmina929@gmail.com>
2020-05-10 16:15:58 +0800

    计算等级数据

--- utils/sql_ops.py
+++ utils/sql_ops.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 class Sqlite():
     def __init__(self, db):
@@ -36,3 +37,75 @@ class Sqlite():
     def close(self):
         self.c.close()
         self.conn.close()
+
+
+class SqlOps():
+    def __init__(self):
+        self.conn = sqlite3.connect('starlightRe.db')
+        self.c = self.conn.cursor()
+    
+    def create(self, cols, types, primary):
+        ct = []
+        for c, t in zip(cols, types):
+            ct.append("{} {}".format(c, t))
+        sqls = 'CREATE TABLE IF NOT EXISTS {} ({}, PRIMARY KEY ({}))'.format(self.table, ','.join(ct), primary)
+        try: 
+            self.c.execute(sqls)
+        except Exception as e:
+            print("error: ", e)
+            return 0            
+        return 1
+    
+    def insert(self, data):
+        sqls = 'INSERT OR IGNORE INTO ' + self.table + ' VALUES ({}?)'.format('?,'*(len(data[0]) - 1))
+        print(sqls)
+        try:
+            self.c.executemany(sqls, iter(data))
+            self.conn.commit()
+        except Exception as e:
+            print("error: ", e)
+            self.conn.rollback()
+    
+    def find_all(self):
+        sqls = 'SELECT * FROM {}'.format(self.table)
+        results = self.c.execute(sqls)
+        return results
+    
+    def update(self):
+        pass
+    
+    def delete(self):
+        pass
+
+
+class LevelOps(SqlOps):
+    def __init__(self):
+        super(LevelOps, self).__init__()
+        self.table = 'Level'
+        self.clos = ''
+    
+    def find_all(self):
+        sqls = 'SELECT * FROM {}'.format(self.table)
+        df = pd.read_sql_query(sqls, self.conn)
+        return df
+    
+    def update(self, data):
+        sqls = 'REPLACE INTO {} VALUES({}?)'.format(self.table, '?,'*(len(data[0]) - 1))
+        print(sqls)
+        try:
+            self.c.executemany(sqls, iter(data))
+            self.conn.commit()
+        except Exception as e:
+            print("error: ", e)
+            self.conn.rollback()
+
+
+class StrengthOps(SqlOps):
+    def __init__(self):
+        super(StrengthOps, self).__init__()
+        self.table = 'strength'
+    
+    def find_all(self):
+        sqls = 'SELECT * FROM {}'.format(self.table)
+        df = pd.read_sql_query(sqls, self.conn)
+        return df
